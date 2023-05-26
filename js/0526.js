465d2c1d2fff2bbb911210facd4ac934d982cbe0
KimJaeHyeok <master_catch@naver.com>
2023-05-26 16:34:03 +0900

    2023.05.26 JH

---
+++ js/0526.js
@@ -0,0 +1,27 @@
+// 논리비교연산자
+let a = 1;
+let b = "1";
+console.log(typeof a);
+console.log(typeof b);
+console.log(a==b);
+console.log(a===b);
+
+console.log('=======================================');
+
+let c = true;
+let d = true;
+console.log(c === d);
+
+console.log('=======================================');
+
+console.log('대한민국만세입니다'[3]);
+console.log('대한민국만세입니다'[0]);
+console.log('대한민국만세입니다'[5]);
+console.log('대한민국만세입니다'[2]);
+console.log('대한민국만세입니다'[7]);
+console.log('대한민국만세입니다'[13]);
+
+console.log('=======================================');
+
+console.log("\"안녕하세요\"");
+console.log("`안녕하세요`");
